22a35d0f4847396db9ad89fd830a91cdf4f21780
Alan Steremberg <alan.steremberg@gmail.com>
2021-01-24 21:33:01 -0500

    state machine fixes

--- verilator_imgui/hiscore.sv
+++ verilator_imgui/hiscore.sv
@@ -8,7 +8,8 @@ module hiscore (
    input [7:0] ioctl_din,
    input [7:0]  ioctl_index,
    output [9:0] ram_address,
-   output [7:0] data_to_ram
+   output [7:0] data_to_ram,
+   output  reg ram_write
 );
 
 /*
@@ -50,6 +51,8 @@ begin
         $display("HISCORE ioctl_addr %x %b ioctl_dout %x ioctl_dout_r %x", ioctl_addr,ioctl_addr[2:0],ioctl_dout,ioctl_dout_r);
     if (ioctl_download & ioctl_wr & ~ioctl_addr[2] &  ioctl_addr[1] & ioctl_addr[0])
         $display("HI HISCORE ioctl_addr %x %b ioctl_dout_r2 %x ioctl_dout_r3 %x ioctl_dout ", ioctl_addr,ioctl_addr[2:0],ioctl_dout_r2,ioctl_dout_r3,ioctl_dout);
+    if (ioctl_download & ioctl_wr & ioctl_addr[2] &  ioctl_addr[1] & ~ioctl_addr[0] &(ioctl_index==3))
+        $display("ENDVAL ioctl_addr %x %b counter %x ioctl_dout %x ", ioctl_addr,ioctl_addr[2:0],ioctl_addr[6:3],ioctl_dout);
 end
 
 dpram#(
@@ -91,6 +94,7 @@ enddata_table(
 .clock_a(clk),
 .data_a(ioctl_dout),
 .wren_a(ioctl_download& ioctl_wr & ioctl_addr[2] & ioctl_addr[1] & ~ioctl_addr[0]& (ioctl_index==3)), // ADDR b110
+.clock_b(clk),
 .q_b(end_val),
 .address_b(counter)
 );
@@ -100,7 +104,7 @@ reg [24:0]old_io_addr;
 reg [24:0]  base_io_addr;
 reg [24:0] end_addr;
 
-reg [1:0] state = 2'b0;
+reg [2:0] state = 3'b0;
 
 reg [24:0] local_addr;
 // 
@@ -132,51 +136,91 @@ end
 else if (done_downloading==1'b1 && ioctl_upload==1'b0) begin
    case (state)
        // start with counter == 0?
-       2'b00: // start
+       3'b000: // start
          // setup beginning addr 
           begin
-             state = 2'b01;
+             $display("state 0 ram_addr %x addr_base %x",ram_addr,addr_base);
+             state <= 3'b001;
              local_addr<=25'b0;
-              base_io_addr<=25'b0;
+             base_io_addr<=25'b0;
              ram_addr<= addr_base;
           end
-       2'b01:
+       3'b001:
+           begin
+             ram_addr<= addr_base;
+              $display("HI HISCORE ?start_val==ioctl_din ioctl_din %x start_val %x ram_addr %x addr_base %x counter %x",ioctl_din,start_val,ram_addr,addr_base,counter);
          // check beginning
              if (ioctl_din==start_val)
              begin
                  $display("HI HISCORE start_val==ioctl_din");
-		state = 2'b10;
+		state <= 3'b010;
                 // setup ending addr 
                 ram_addr<= end_addr;
              end
+         end
         // check end
-       2'b10:
-              $display("HI HISCORE ?end_val==ioctl_din ioctl_din %x end_val %x ram_addr %x",ioctl_din,end_val,ram_addr);
+       3'b010:
+	     begin
+             // $display("HI HISCORE ?end_val==ioctl_din ioctl_din %x end_val %x ram_addr %x counter %x",ioctl_din,end_val,ram_addr,counter);
              if (ioctl_din==end_val)
              begin
                  $display("HI HISCORE end_val==ioctl_din");
                    // check to see if we are at the end..
                 if (counter==total_entries)
                 begin
-                   state = 2'b11;
+                   state <= 3'b110;
                    counter<=0;
+		   ram_write<=0;
+                   $display("moving on..");
+           
                 end
                 else begin
+                   $display("try next entry");
 		   counter<=counter+1'b1;
-                   state = 2'b00;
+                   state <= 3'b000;
                 end
              end
-       2'b11:
+             end
+       3'b011:
             begin
                 local_addr<= local_addr+1'b1;
+                $display("DUMP local_addr %x ram_addr %x addr_base %x base_io_addr %x end_addr %x ram_write %x",local_addr,ram_addr,addr_base,base_io_addr,end_addr,ram_write);
                 if (ram_addr==end_addr[24:0])
                 begin
-                   counter<=counter+1'b1;
-                   base_io_addr<=ioctl_addr+1'b1;
+                   if (counter==total_entries) begin
+                     $display("counter==total %x == %x done writing",counter,total_entries);
+                      ram_write<=0;
+                      state<=3'b101;
+                   end else begin
+                     $display("increment counter %x ",counter);
+                     counter<=counter+1'b1;
+                     base_io_addr<=local_addr+1'b1;
+                     state<=3'b100;
+		     ram_write<=0;
+                   end
                 end
                 ram_addr<= addr_base + (local_addr - base_io_addr);
             end
+        3'b100:
+           begin
+               state<=3'b011;
+	       ram_write<=1;
+           end
+        3'b101:
+	   begin
+		ram_write<=0;
+           end
+        3'b110:
+	   begin
+		state<=3'b100;
+                ram_addr<= addr_base;
+           end
+
    endcase
+
+if (ram_write)
+    $display("RAMWRITE: local_addr %x ram_addr %x ram_write %x data_to_ram %x",local_addr,ram_addr,ram_write,data_to_ram);
+ 
 end
  
 old_io_addr<=ioctl_addr;
@@ -196,8 +240,9 @@ hiscoredata (
 .clock_a(clk),
 .data_a(ioctl_dout),
 .wren_a(ioctl_download & ioctl_wr & (ioctl_index==4)), 
+.clock_b(clk),
 .q_b(data_to_ram),
-.address_b(local_addr)
+.address_b(local_addr[4:0])
 );
 
 reg [24:0] last_address;
